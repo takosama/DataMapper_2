0bc9746c9487463bb410e18cf7d92f1c35556924
tako <takosama.312@gmail.com>
2017-10-31 18:12:22 +0900

    test

--- DataMapper_2/Class1.cs
+++ DataMapper_2/Class1.cs
@@ -6,7 +6,52 @@ using System.Threading.Tasks;
 
 namespace DataMapper_2
 {
-    public class Class1
+    class Mapper
     {
+        public static T MapDynamic<T>(dynamic obj) where T : new()
+        {
+            var rtn = new T();
+
+            foreach (var n in obj)
+            {
+                var type = n.Key;
+                var val = n.Value;
+                try
+                {
+                    rtn.GetType().GetProperty(type).SetValue(rtn, val);
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+            }
+            return rtn;
+        }
+
+        static public T MapDynamicLiteralObjectEntity<T>(JsDataParser.Entities.IDynamicLiteralObjectEntity obj) where T : new()
+        {
+            var rtn = new T();
+            foreach (var n in rtn.GetType().GetProperties())
+            {
+                string name = n.Name;
+                if (!obj.TryGetField(name, out dynamic tmp)) continue;
+
+                var val = (JsDataParser.Entities.ValueEntity)tmp;
+
+                object setObj;
+                if (val.ValueType == JsDataParser.Entities.ValueTypes.Array)
+                {
+                    setObj = val.Array.Select(x => x.Object).ToArray();
+                }
+                else
+                    setObj = val.Object;
+                rtn.GetType().GetProperty(name).SetValue(rtn, setObj);
+            }
+            return rtn;
+        }
+
+
     }
+
+
 }
